a0e9b615f5739010697b85030bbba3576fbe1404
Jesse Liberty <jesse.liberty@non.se.com>
2020-10-27 07:22:51 -0400

    add another line

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("a new line");
         }
     }
 }
