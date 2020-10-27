e5c7f2ecabf5c59cec873075ba5c9bc122b9d7a2
Jesse Liberty <jesse.liberty@non.se.com>
2020-10-27 07:20:47 -0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        protected Program()
+        {
+            // required
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
